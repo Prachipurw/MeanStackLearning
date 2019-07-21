42efc15a13d1f3ab934f5bb395478972d077ba58
Prachipurw <prachipurwar08@gmail.com>
2019-07-21 12:07:41 +0530

    My first commmit

---
+++ Programming/name.js
@@ -0,0 +1,13 @@
+let multiplyMagic = (a=1,b=1)=>{
+	//console.log(multiplyMagic.length)
+let c=0;
+ if(a==1 && b==1)
+  {  
+  c = 1;
+  }
+  else{ 
+	 c=a*b;
+  }
+  console.log(c)
+};
+multiplyMagic(2,3,4)
